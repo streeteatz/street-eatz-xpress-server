4d51cd0a689778df99150cbb31783e2f8323f20d
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-07-11 16:22:20 -0400

    Change the cors address

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`)
 
   socket.on("send_message", (data) => {
+    console.log(data)
     socket.broadcast.emit("receive_message", data)
   })
 })
