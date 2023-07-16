960ae20685c2d9964982ec73df014a5aecea61f1
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-07-16 11:13:22 -0400

    Change cors to accept the deployed FE domain

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const { Server } = require('socket.io')
 const cors = require('cors')
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://streateatz-fe.vercel.app/"
 }))
 const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://streateatz-fe.vercel.app/",
     allowedHeaders: ["street-eat"],
     credentials: true,
     methods: ["GET", "POST"]
